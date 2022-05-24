2ce699e071878ae87d601fdac5a334499549b677
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 22:57:20 +0530

    Connection string added in config created new classes

---
+++ ADODotNetDemo/CallStoredProcedureDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace ADODotNetDemo
+{
+    internal class CallStoredProcedureDemo
+    {
+        class Employee
+        {
+            public int EmpID { get; set; }
+            public string EmpName { get; set; }
+            public string EmpLoc { get; set; }
+        }
+
+        class ClsProcedureCall
+        { 
+        
+        
+        }
+            static void Main(string[] args)
+        {
+
+        }
+    }
+}
