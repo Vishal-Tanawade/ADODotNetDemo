5702d49e4933f026d1e30cdf632592d9d92d5573
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 19:44:08 +0530

    interface added IEmployeeDatabaseML

--- ADODotNetDemo/ClsEmployee.cs
+++ ADODotNetDemo/ClsEmployee.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace ADODotNetDemo
 {
+    interface IEmployeeDatabaseML
+    {
+        void AllEmployeeDetails();
+        void AddEmployee(ClsEmployee Employee);
+        void UpdateEmployee();
+        void DeleteEmployee();
+    }
+
+
     internal class ClsEmployee
     {
         public int EmpID { get; set; }
