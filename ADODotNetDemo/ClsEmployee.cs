468c578a8ee1be5f80a7892581ebc53b89c9429f
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 19:55:34 +0530

    details from database fetched

--- ADODotNetDemo/ClsEmployee.cs
+++ ADODotNetDemo/ClsEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -34,7 +35,29 @@ namespace ADODotNetDemo
 
         public void AllEmployeeDetails()
         {
-            throw new NotImplementedException();
+            //com = new SqlCommand("select * from tblEmployee", con);
+           //Or
+            com = new SqlCommand();
+            com.CommandText = "select * from tblEmployee";
+            com.Connection = con;
+
+            dr = com.ExecuteReader();
+            int counter = 1;
+            Console.WriteLine("EMPLOYEE DETAILS ARE AS FOLLOWS:");
+            while (dr.Read())
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} Employee details :", counter);
+                Console.WriteLine("EmpID   : {0}", dr[0].ToString());
+                Console.WriteLine("EmpName : {0}", dr["EmpName"].ToString());
+                Console.WriteLine("DeptID  : {0}", dr["DeptID"].ToString());
+                Console.WriteLine();
+                counter++;
+
+            }
+            dr.Close();
+            com.Dispose(); // it will close connection after closing application
+            // as we can close connection using con.close() but what if other function wants to use the connection
         }
 
         public void DeleteEmployee()
