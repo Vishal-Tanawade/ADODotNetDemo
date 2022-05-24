2110458b78be6888224d71623ecd0b1dfd65fb7b
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 19:43:05 +0530

    Created two classes 1) ADODBMLDemo 2) ClsEmployee

---
+++ ADODotNetDemo/ADODBMLDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;  //API
+namespace ADODotNetDemo
+{
+    namespace ADODotNetDemo
+{
+    internal class ADODBMLDemo
+    {
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
---
+++ ADODotNetDemo/ClsEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADODotNetDemo
+{
+    internal class ClsEmployee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int DeptID { get; set; }
+
+    }
+}
