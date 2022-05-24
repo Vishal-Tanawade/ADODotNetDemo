6cf2824154214a4f77b6dfaa6f9c9a7bde1a7b6c
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 23:50:04 +0530

    OleDb added

---
+++ ADODotNetDemo/DirectTableDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace ADODotNetDemo
+{
+    internal class DirectTableDemo
+    {
+        static void Main(string[] args)
+        {
+            string mycon = ConfigurationManager.ConnectionStrings["mycon2"].ConnectionString;
+            OleDbConnection conn = new OleDbConnection();
+            // new OleDbConnection(@"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=GN22ADMDNF001;Data Source=DESKTOP-FCE1AML");
+            conn.ConnectionString = mycon;
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "tblEmployeeWithSalary";
+            cmd.CommandType = CommandType.TableDirect;
+
+            OleDbDataReader dr = cmd.ExecuteReader();
+            int count = 1;
+            Console.WriteLine("Employees details are as follows :");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}. Employee details :", count);
+                Console.WriteLine("Employee ID         : {0}", dr["EmpID"].ToString());
+                Console.WriteLine("Employee Name       : {0}", dr["EmpName"].ToString());
+                Console.WriteLine("Employee DeptID     : {0}", dr["DeptID"].ToString());
+                Console.WriteLine("Employee Salary     : {0}", dr["Salary"].ToString());
+
+                count++;
+                Console.WriteLine();
+
+            }
+            dr.Close();
+            cmd.Dispose();
+            conn.Close();
+
+        }
+    }
+}
