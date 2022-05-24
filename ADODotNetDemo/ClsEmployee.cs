3370e5ebcde53ec8ab48fc36fca784f1e45ff345
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-24 19:46:35 +0530

    Interface Implemented Connection String Added

--- ADODotNetDemo/ClsEmployee.cs
+++ ADODotNetDemo/ClsEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,39 @@ namespace ADODotNetDemo
         void AddEmployee(ClsEmployee Employee);
         void UpdateEmployee();
         void DeleteEmployee();
+    }
+    class EmployeeDatabaseML : IEmployeeDatabaseML
+    {
+
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader dr;
+        public EmployeeDatabaseML()
+        {
+            con = new SqlConnection(@"Data Source=DESKTOP-FCE1AML;Initial Catalog=GN22ADMDNF001;Integrated Security = true");
+
+            con.Open();
+        }
+
+        public void AddEmployee(ClsEmployee Employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AllEmployeeDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 
